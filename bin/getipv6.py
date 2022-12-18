8f0fc71ab8b702f0a664159d76f05483b59f90fb
Muhammad Habib Ulil A <habibulilalbaab@intprism.com>
2022-12-18 13:09:11 +0700

    Update getipv6.py

--- bin/getipv6.py
+++ bin/getipv6.py
@@ -1,9 +1,14 @@
 def result():
     import ipaddress
     import subprocess
-    
-    direct_output = subprocess.check_output('ip a | grep "inet6 2"', shell=True)
-    result_ip = direct_output.decode("utf-8").split("    inet6 ")[1].split("/")[0]
+    notGet = True
+    while(notGet):
+        try:
+            direct_output = subprocess.check_output('ip a | grep "inet6 2"', shell=True)
+            result_ip = direct_output.decode("utf-8").split("    inet6 ")[1].split("/")[0]
+            notGet = False
+        except:
+            print("[WARN] Try to get IPv6")
     if(ipaddress.ip_address(result_ip).is_global != True):
         return "failed"
     return result_ip
